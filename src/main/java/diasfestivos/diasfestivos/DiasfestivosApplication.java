7681af011750013786648e8b3a20393f1acf07df
Sebastian Londoño <juan.londono29@udea.edu.co>
2023-10-16 15:59:02 -0500

    :zap: init the project

---
+++ src/main/java/diasfestivos/diasfestivos/DiasfestivosApplication.java
@@ -0,0 +1,13 @@
+package diasfestivos.diasfestivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiasfestivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiasfestivosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/diasfestivos/diasfestivos/DiasfestivosApplicationTests.java
@@ -0,0 +1,13 @@
+package diasfestivos.diasfestivos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiasfestivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
