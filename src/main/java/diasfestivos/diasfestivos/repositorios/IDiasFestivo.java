2676ae89f1a90063174ae3c03a4d0185218d1a5c
Sebastian Londoño <juan.londono29@udea.edu.co>
2023-10-16 17:24:04 -0500

    :art: add interface

---
+++ src/main/java/diasfestivos/diasfestivos/repositorios/IDiasFestivo.java
@@ -0,0 +1,10 @@
+package diasfestivos.diasfestivos.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import diasfestivos.diasfestivos.entidades.Festivo;
+
+public interface IDiasFestivo extends JpaRepository<Festivo, Long>{
+
+}
+
